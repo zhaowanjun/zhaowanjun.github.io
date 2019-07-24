c907c4c3d4ddeb580c8eec2619ee6b7413ae1cbf
zhaowanjun <zhaowanj@gmail.com>
2019-07-24 23:39:02 +0800

    修改birthday页面的文字

--- Happy-Birthday/js/functions_dev.js
+++ Happy-Birthday/js/functions_dev.js
@@ -111,7 +111,7 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+	var result = "<span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#elapseClock").html(result);
 }
 
